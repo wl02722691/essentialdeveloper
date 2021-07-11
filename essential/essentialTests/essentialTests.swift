317c4836f95666a5de1d9b728982e68da9ab9f13
Alice <wl02722691@gmail.com.tw>
2021-07-12 03:24:33 +0800

    Simpify spy

--- essential/essentialTests/essentialTests.swift
+++ essential/essentialTests/essentialTests.swift
@@ -12,7 +12,7 @@ class essentialTests: XCTestCase {
 
     func test_init_doesNotRequestDataFromURL() {
         let (_, client) = makeSUT()
-        XCTAssertNil(client.requestedURL)
+        XCTAssertTrue(client.requestedURLs.isEmpty)
     }
 
     func test_load_requestsDataFromURL() {
@@ -21,7 +21,7 @@ class essentialTests: XCTestCase {
         
         sut.load()
         
-        XCTAssertEqual(client.requestedURL, url)
+        XCTAssertEqual(client.requestedURLs, [url])
     }
     
     func test_loadTwice_requestsDataFromURL() {
@@ -43,7 +43,6 @@ class essentialTests: XCTestCase {
     }
     
     private class HTTPClintSpy: HTTPClient {
-        var requestedURL: URL?
         var requestedURLs = [URL]()
 
         func get(from url: URL) {
