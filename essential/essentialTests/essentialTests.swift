77042c09549291b4afdfb5ca19ea19610dbfaa0a
Alice <wl02722691@gmail.com.tw>
2021-07-12 14:34:19 +0800

    Delivers connectivity error on client error

--- essential/essentialTests/essentialTests.swift
+++ essential/essentialTests/essentialTests.swift
@@ -34,6 +34,16 @@ class essentialTests: XCTestCase {
         XCTAssertEqual(client.requestedURLs, [url, url])
     }
     
+    func test_load_deliversErrorOnClientError() {
+        let (sut, client) = makeSUT()
+        client.error = NSError(domain: "Test", code: 0)
+        
+        var capturedError: RemoteFeedLoader.Error?
+        
+        sut.load { error in capturedError = error }
+        XCTAssertEqual(capturedError, .connectivity)
+    }
+    
     // MARK - Helpers
     
     private func makeSUT(url: URL = URL(string: "https://a-url.com")!) -> (sut: RemoteFeedLoader, client: HTTPClintSpy) {
@@ -44,9 +54,12 @@ class essentialTests: XCTestCase {
     
     private class HTTPClintSpy: HTTPClient {
         var requestedURLs = [URL]()
-
-        func get(from url: URL) {
-            requestedURL = url
+        var error: Error?
+        
+        func get(from url: URL, completion: @escaping (Error) -> Void) {
+            if let error = error {
+                completion(error)
+            }
             requestedURLs.append(url)
         }
     }
