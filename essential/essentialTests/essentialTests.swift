e8046c62ca60862eb8b827a35f634a78695727a1
Alice <wl02722691@gmail.com.tw>
2021-07-12 15:30:07 +0800

    Combine spy captured params into a single type

--- essential/essentialTests/essentialTests.swift
+++ essential/essentialTests/essentialTests.swift
@@ -55,16 +55,18 @@ class essentialTests: XCTestCase {
     }
     
     private class HTTPClintSpy: HTTPClient {
-        var requestedURLs = [URL]()
-        var completions = [(Error) -> Void]()
+        private var messages = [(url: URL, completions: (Error) -> Void)]()
+        
+        var requestedURLs: [URL] {
+            return messages.map { $0.url }
+        }
         
         func get(from url: URL, completion: @escaping (Error) -> Void) {
-            completions.append(completion)
-            requestedURLs.append(url)
+            messages.append((url, completion))
         }
         
         func complete(with error: Error, at index: Int = 0) {
-            completions[index](error)
+            messages[index].completions(error)
         }
     }
 }
