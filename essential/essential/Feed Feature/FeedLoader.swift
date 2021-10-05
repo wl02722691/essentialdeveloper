56c17841e5ae87a9cf73b00d46524e50938409e1
alice <wl02722691@github.com>
2021-10-05 20:01:51 +0800

    Remove 'LoadFeedResult' Equatable conformance from production since it's a test-only requirement

--- essential/essential/Feed Feature/FeedLoader.swift
+++ essential/essential/Feed Feature/FeedLoader.swift
@@ -9,9 +9,6 @@ public enum LoadFeedResult<Error: Swift.Error> {
 	case failure(Error)
 }
 
-// 特別的寫法 如何讓 Error Equatable
-extension LoadFeedResult: Equatable where Error: Equatable {}
-
 protocol FeedLoader {
     associatedtype Error: Swift.Error
 	func load(completion: @escaping (LoadFeedResult<Error>) -> Void)
--- essential/essentialTests/Feed API/essentialTests.swift
+++ essential/essentialTests/Feed API/essentialTests.swift
@@ -144,13 +144,27 @@ class essentialTests: XCTestCase {
         return try! JSONSerialization.data(withJSONObject: itemsJSON)
     }
     
-    private func expect(_ sut: RemoteFeedLoader, toCompleteWith result: RemoteFeedLoader.Result , when action: ()->Void, file: StaticString = #filePath, line: UInt = #line ) {
-        var capturedResults = [RemoteFeedLoader.Result]()
-        sut.load { capturedResults.append($0) }
+    private func expect(_ sut: RemoteFeedLoader, toCompleteWith expectedResult: RemoteFeedLoader.Result , when action: () -> Void, file: StaticString = #filePath, line: UInt = #line ) {
         
+        let exp = expectation(description: "Wait for load completion")
+        
+        sut.load { receivedResult in
+            switch (receivedResult, expectedResult) {
+            case let (.success(receivedItems), .success(expectedItems)):
+                XCTAssertEqual(receivedItems, expectedItems, file: file, line: line)
+            case let (.failure(receivedItems), .failure(expectedItems)):
+                XCTAssertEqual(receivedItems, expectedItems, file: file, line: line)
+            default:
+                XCTFail("Expected result \(expectedResult) got \(receivedResult) instead", file: file, line: line)
+                
+            }
+            
+            exp.fulfill()
+            
+        }
         action()
         
-        XCTAssertEqual(capturedResults, [result], file: file, line: line)
+        wait(for: [exp], timeout: 1.0)
     }
     
     private class HTTPClintSpy: HTTPClient {
