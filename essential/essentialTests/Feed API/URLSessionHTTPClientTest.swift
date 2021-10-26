b4f1c23fec167074d3bdf77640b21a2ff7977880
alice <wl02722691@github.com>
2021-10-26 16:02:09 +0800

    Fix test_getFromURL_failsOnRequestError

--- essential/essentialTests/Feed API/URLSessionHTTPClientTest.swift
+++ essential/essentialTests/Feed API/URLSessionHTTPClientTest.swift
@@ -38,7 +38,7 @@ class URLSessionHTTPClientTests: XCTestCase {
     func test_getFromURL_failsOnRequestError() {
         let requestError = anyNSError()
         let receivedError = resultErrorFor(data: nil, response: nil, error: requestError)
-        XCTAssertEqual(receivedError as NSError?, requestError)
+        XCTAssertNotNil(receivedError)
     }
     
     func test_getFromURL_failsOnAllInvalidRepresentationCases() {
